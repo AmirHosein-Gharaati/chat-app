5457ba86e02bac9096ac42a0b03450f4d640b53c
amirhosein <amirhosein@cyberdesign.se>
2024-12-18 09:31:40 +0330

    feat: create a module for message

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatModule } from './modules/chat/chat.module';
 import { RoomModule } from './modules/room/room.module';
+import { MessageModule } from './modules/message/message.module';
 
 @Module({
-  imports: [ChatModule, RoomModule],
+  imports: [ChatModule, RoomModule, MessageModule],
   controllers: [],
   providers: [],
 })
