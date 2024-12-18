4381d9b47eb2ee3d1b618c809183431d58208a4a
amirhosein <amirhosein@cyberdesign.se>
2024-12-18 17:47:29 +0330

    feat: add logs to services

--- src/modules/message/message.service.ts
+++ src/modules/message/message.service.ts
@@ -1,10 +1,12 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { Message } from './message.entity';
 import { RoomService } from '../room/room.service';
 import { MessageRepository } from './repo/message.repository';
 
 @Injectable()
 export class MessageService {
+  private readonly logger = new Logger('MessageService');
+  
   constructor(
     private readonly roomService: RoomService,
     private readonly messageRepository: MessageRepository,
@@ -13,18 +15,22 @@ export class MessageService {
   async create(text: string, userId: string, roomId: string) {
     const userExistsInRoom = await this.roomService.participantExists(roomId, userId);
     if (!userExistsInRoom) {
+      this.logger.error(`user does not exist with roomId=${roomId}, userId=${userId}`);
       throw new NotFoundException(
         `user does not exist with roomId=${roomId}, userId=${userId}`,
       );
     }
     
     const message = new Message(text, userId, roomId);
+    this.logger.log(`message created with id=${message.id}`);
+    
     return await this.messageRepository.save(message);
   }
 
   async retrieveMessages(roomId: string) {
     const roomExists = await this.roomService.existsById(roomId);
     if (!roomExists) {
+      this.logger.error(`room does not exist with id=${roomId}`);
       throw new NotFoundException(`room does not exist with id=${roomId}`);
     }
 
--- src/modules/room/room.service.ts
+++ src/modules/room/room.service.ts
@@ -1,21 +1,28 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { Room } from './room.entity';
 import { RoomRepository } from './repo/room.repository';
 
 @Injectable()
 export class RoomService {
+  private readonly logger = new Logger('RoomService');
+
   constructor(private readonly roomRepository: RoomRepository) {}
 
   create(name: string) {
     const room = new Room(name);
+    this.logger.log(`creating room with id=${room.id}`);
+
     return this.roomRepository.save(room)
   }
 
   async addParticipant(roomId: string, userId: string) {
     const room = await this.roomRepository.findById(roomId);
     if (!room) {
+      this.logger.error(`room does not exist with id=${roomId}`);
       throw new NotFoundException(`room does not exist with id=${roomId}`);
     }
+
+    this.logger.log(`adding participant to room with id=${roomId}, userId=${userId}`);
 
     room.participants.push(userId);
     return this.roomRepository.update(room);
@@ -25,6 +32,7 @@ export class RoomService {
     const room = await this.roomRepository.findById(roomId);
     
     if (!room) {
+      this.logger.error(`room does not exist with id=${roomId}`);
       throw new NotFoundException(`room does not exist with id=${roomId}`);
     }
 
